7319ac124ac6a7e1018434111df41318643616d2
rooslunn <rooslunn@gmail.com>
2015-08-06 14:30:00 +0300

    Initial commit

---
+++ LinkSpeed/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LinkSpeed
+//
+//  Created by Russ on 8/6/15.
+//  Copyright (c) 2015 Russ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :osx, "10.10"
+use_frameworks!
+
+target 'LinkSpeed' do
+    pod 'Alamofire', '1.3.0'
+end
+
+target 'LinkSpeedTests' do
+
+end
+
